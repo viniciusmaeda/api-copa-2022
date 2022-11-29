48ea5411bb782cbaf30dbbbb1d28cc94f21325e3
Vinícius Maeda <viniciusmaeda@gmail.com>
2022-11-29 17:44:24 -0400

    dot env e put

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+// utilização do dotenv para leitura dos dados privados
+require("dotenv").config();
+
 // instanciar o módulo express
 const app = express();
+
+// habilita o recebimento de dados json
+app.use(express.json());
 
 // importar as rotas da aplicação
 const groupRoutes = require("./routes/Group");
@@ -15,10 +21,12 @@ app.use("/team", teamRoutes);
 
 // conexão com o banco de dados MongoDB (Atlas)
 mongoose
-  .connect("")
+  .connect(
+    `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.qxdwo.mongodb.net/${process.env.DB}`,
+  )
   .then(() => {
     console.log("Database connected!");
-    app.listen(3000);
+    app.listen(process.env.PORT);
   })
   .catch((err) => {
     console.log(err);
--- routes/Team.js
+++ routes/Team.js
@@ -1,12 +1,27 @@
 // importar o módulo express
 const express = require("express");
 
+// importar o modelo
+const Team = require("../models/TeamModel");
+
 // método Router para lidar com as rotas
 const routes = express.Router();
 
 // GET - Obter os dados dos times
 routes.get("/", (req, res) => {
   res.send("Esta rota irá retornar os times.");
+});
+
+// POST - Salvar um novo time no BD
+routes.post("/", async (req, res) => {
+  // obter os dados através da requisição
+  const time = req.body;
+
+  await Team.create(time);
+
+  console.log(time);
+
+  res.send("teste");
 });
 
 // exportar as rotas para veículo
