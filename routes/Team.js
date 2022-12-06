a86421d6f65f7bad4bdf9ab8f2495c91dda4c4bd
Vinícius Maeda <viniciusmaeda@gmail.com>
2022-12-06 17:14:15 -0400

    consulta e inserção no db

--- routes/Team.js
+++ routes/Team.js
@@ -8,20 +8,34 @@ const Team = require("../models/TeamModel");
 const routes = express.Router();
 
 // GET - Obter os dados dos times
-routes.get("/", (req, res) => {
-  res.send("Esta rota irá retornar os times.");
+routes.get("/", async (req, res) => {
+  try {
+    // constante para receber os dados do banco
+    const teams = await Team.find();
+
+    // enviar os dados da consulta ao usuário
+    res.status(200).json(teams);
+  } catch (error) {
+    // se houver erro na consulta, devolve a mensagem de erro
+    res.status(500).json({ error: error });
+  }
 });
 
 // POST - Salvar um novo time no BD
 routes.post("/", async (req, res) => {
-  // obter os dados através da requisição
-  const time = req.body;
+  // obter os dados do corpo da requisição
+  const team = req.body;
 
-  await Team.create(time);
+  try {
+    // realiza a criação dos dados no BD
+    await Team.create(team);
 
-  console.log(time);
-
-  res.send("teste");
+    // retorna com mensagem de sucesso e o código de criação
+    res.status(201).send("Time inserido com sucesso");
+  } catch (error) {
+    // se houver erro na consulta, devolve a mensagem de erro
+    res.status(500).json({ error: error });
+  }
 });
 
 // exportar as rotas para veículo
